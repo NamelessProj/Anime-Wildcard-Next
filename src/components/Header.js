79b565ea242fee1fe211e42e1e3a1894ed982971
Kevin <pintokevin2002@hotmail.com>
2025-02-23 11:18:01 +0100

    Updating the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import {FaGithub} from "react-icons/fa";
+
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className="p-3 flex justify-between items-center">
+            <p className="text-3xl">
+                <Link href="/">
+                    Anime Wildcard
+                </Link>
+            </p>
+
+            <a href="https://github.com/NamelessProj/Anime-Wildcard-Next" target="_blank">
+                <FaGithub size={27} color="white" />
+            </a>
+        </header>
     );
 };
 
