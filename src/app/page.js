89ced44ea76023cec86655f381255bb6bd939da7
Kevin <pintokevin2002@hotmail.com>
2025-02-23 14:46:53 +0100

    Displaying the first stage to the user

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,37 @@
+'use client';
+
+import React from "react";
+import UserForm from "@/components/UserForm";
+
 export default function Home() {
-  return (
-    <main>
-      <p>
-        lol
-      </p>
-    </main>
-  );
+    const [username, setUsername] = React.useState('');
+    const [getAdultContent, setGetAdultContent] = React.useState(false);
+    const [error, setError] = React.useState('');
+    const [stage, setStage] = React.useState(0);
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        setError('');
+        if(!username || username === ""){
+            setError("Please enter your Anilist username.");
+            document.querySelector('input[name="username"]').focus();
+            return;
+        }
+        setStage(stage + 1);
+    }
+
+    return (
+        <>
+            {stage === 0 && (
+                <UserForm
+                    username={username}
+                    setUsername={setUsername}
+                    getAdultContent={getAdultContent}
+                    setGetAdultContent={setGetAdultContent}
+                    handleNext={handleNext}
+                    error={error}
+                />
+            )}
+        </>
+    );
 }
