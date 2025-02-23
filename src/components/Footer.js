a26af529a39b8e281501dae782d04e648f9e7031
Kevin <pintokevin2002@hotmail.com>
2025-02-23 11:23:35 +0100

    Updating the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
+import {format} from "date-fns";
+
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer className="p-2">
+            <div>
+                <p className="text-center text-sm text-balance text-gray-400">
+                    &copy; {format(new Date(), "MMM yyyy")} - All Rights Reserved - <a className="text-primary-400 opacity-80 hover:opacity-100" target="_blank" href="https://github.com/NamelessProj">Da Silva Pinto Kevin</a>
+                </p>
+            </div>
+        </footer>
     );
 };
 
