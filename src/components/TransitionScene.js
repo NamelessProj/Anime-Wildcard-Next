ab10d103013d773d7dff337a9305d222dc58c319
Kevin <pintokevin2002@hotmail.com>
2025-02-26 18:45:02 +0100

    Adding a scene to make a transition to the final result screen

---
+++ src/components/TransitionScene.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TransitionScene = () => {
+    return (
+        <div className="transition-scene-container fixed top-0 left-0 w-svw h-svh z-50 overflow-clip">
+            <div className="transition-scene relative flex justify-center items-center h-full w-svw">
+                <p className="text-center text-3xl text-balance">
+                    Your final result is ready!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default TransitionScene;
