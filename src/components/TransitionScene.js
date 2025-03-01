f1145ad53ffb4457064b21c13fa18687b8987cb1
Kevin <pintokevin2002@hotmail.com>
2025-03-01 22:18:40 +0100

    Updating the text on the transition scene

--- src/components/TransitionScene.js
+++ src/components/TransitionScene.js
@@ -2,8 +2,8 @@ const TransitionScene = () => {
     return (
         <div className="transition-scene-container fixed top-0 left-0 w-svw h-svh z-50 overflow-clip">
             <div className="transition-scene relative flex justify-center items-center h-full w-svw">
-                <p className="text-center text-3xl text-balance">
-                    Your final result is ready!
+                <p className="text-center text-4xl font-bold text-balance">
+                    Let's see your final top!
                 </p>
             </div>
         </div>
