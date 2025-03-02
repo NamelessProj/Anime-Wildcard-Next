8cbca6ce3753e20a7721bf522088c925b0ec0ca0
Kevin Da Silva Pinto <pintokevin2002@hotmail.com>
2025-03-02 13:37:53 +0100

    Update TransitionScene.js
    Making the transition scene the full height and width of the screen

--- src/components/TransitionScene.js
+++ src/components/TransitionScene.js
@@ -4,7 +4,7 @@ const TransitionScene = ({transitionSceneDuration}) => {
     }
 
     return (
-        <div className="transition-scene-container fixed top-0 left-0 w-svw h-svh z-50 overflow-clip" style={style}>
+        <div className="transition-scene-container fixed top-0 left-0 w-dvw h-dvh z-50 overflow-clip" style={style}>
             <div className="transition-scene relative flex justify-center items-center h-full w-svw">
                 <p className="text-center text-4xl font-bold text-balance">
                     Let's see your final top!
